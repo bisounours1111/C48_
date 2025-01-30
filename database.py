0488b56c4ccdde895bf0a85b414be570365c20d7
bisounours <yan.dai@outlook.fr>
2025-01-30 23:48:40 +0100

    hide info

--- database.py
+++ database.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+IP = os.getenv("IP")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:example@yanisd.pro/postgres"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{USER}:{PASSWORD}@{IP}/postgres"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.secret_key = 'mysecretkey'
+    app.secret_key = SECRET_KEY
 
     db.init_app(app)
 
